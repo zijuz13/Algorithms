d5ad88e6a2e91a3a2f8f062be1aab99653f7090f
Zijun Zhou <70651186+zijuz13@users.noreply.github.com>
2021-02-24 11:54:25 +0800

    Create array_list.h

---
+++ array_list.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct _AList{
+void ** arr;
+int size;
+int maxSize;
+int itemSize;
+char* type;
+} ArrayList;
+
+ArrayList* alist_initialize(int, int, char*);
+bool alist_add(ArrayList*, void*);
+bool alist_add_at(ArrayList*, int, void*);
+void alist_clear(ArrayList*);
+void* alist_get(ArrayList*, int);
+int alist_index_of(ArrayList*, void*);
+void* alist_remove(ArrayList*, int);
+bool alist_destroy(ArrayList*);
+bool alist_resize(ArrayList*);
